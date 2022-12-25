ec835dc7e166bcd750d577440b9a26c780ff4afe
KoudouMizuki <k.mizuki0706@gmail.com>
2022-12-25 09:53:10 +0000

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
 
-  devise_for :users,skip: [:passwords], controllers: {
+  devise_for :users,skip: [:password], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+  devise_for :admin,skip: [:registrations, :password], controllers: {
     sessions: "admin/sessions"
   }
   
   devise_scope :user do
-      post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
+    post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
   end 
   
   scope module: :public do
     root to: "homes#top"
     get 'homes/about' => 'homes#about', as: 'about'
+    resources :users, only: [:show, :edit]
   end  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
