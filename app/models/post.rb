204a1ed2e7d2e8b8cc8c7f63f8aabb00edc0a9dc
KoudouMizuki <k.mizuki0706@gmail.com>
2022-12-27 09:58:03 +0000

    Add

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+    
+    has_one_attached :post_image
   
   enum parking: { yes:0, no:1, others:2}
+  enum season: { spring:0, summer:1, autumm:2, winter:3}
+  enum transportation: { car:0, bike:1, bicycle:2, walking:3}
 end
