2b4e0dcb7c1da54f6c15afe65899cb5b98e8e046
KoudouMizuki <k.mizuki0706@gmail.com>
2023-01-07 09:00:05 +0000

    Update

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -7,11 +7,11 @@ class Public::PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    @tag_list = Tag.all
-    @tag_list = params[:post][:name].split(',')
     if @post.save
-      @post.save_tag(tag_list)
+      @post.save_tags(prams[:post][:tag])
       redirect_to post_path(@post.id)
+    else
+      render :new
     end
   end
 
---
+++ app/models/post.rb
@@ -0,0 +1,27 @@
+class Post < ApplicationRecord
+
+  enum parking: { yes_parking:0, not_parking:1}
+  enum season: { spring:0, summer:1, sutumm:2, winter:3}
+  enum transportation: {car:0, bike:1, bicycle:2, walking:3}
+
+  has_many :post_tag, dependent: :destroy
+  has_many :tags, through: :post_tags
+
+  def save_tags(tags)
+    tag_list = tags.split(/[[:blank]] +/)
+    current_tags = self.tags.pluck(:name)
+    old_tags = current_tags - tag_list
+    new_tags = tag_list - current_tags
+    p current_tags
+
+    old_tags.each do |old|
+      self.tags.delete Tag.find_by(name: old)
+    end
+
+    new_tags.each do |new|
+      new_post_tag = Tag.find_or_create_by(name: new)
+      self.tags << new_post_tag
+    end
+
+  end
+end
