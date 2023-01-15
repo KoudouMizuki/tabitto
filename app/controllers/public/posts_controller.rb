f002c2ace32aaf2430208d5c1951019a062217c2
KoudouMizuki <k.mizuki0706@gmail.com>
2023-01-15 20:15:22 +0000

    Add

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -22,12 +22,19 @@ class Public::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(update_params)
+    redirect_to post_path(@post.id)
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to user_path(current_user)
   end
 
   private
@@ -35,4 +42,8 @@ class Public::PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:post_image, :name, :introduction, :parking, :season, :transportation, :latitude, :longitude)
   end
+  
+  def update_params
+    params.require(:post).permit(:name, :introduction, :parking, :season, :transportation)
+  end  
 end
