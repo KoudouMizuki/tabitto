702e1848814d18952f337f47577f7f8abd212fef
KoudouMizuki <k.mizuki0706@gmail.com>
2023-01-25 14:59:39 +0000

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
- 
+  namespace :admin do
+    get 'comments/index'
+  end
   devise_for :users, controllers: {
     registrations: "public/registrations",
     passwords: 'users/password',
@@ -28,7 +30,12 @@ Rails.application.routes.draw do
     end
     resources :posts do
       resources :comments, only: [:create, :destroy]
-    end  
+    end
+  end
+
+  namespace :admin do
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :comments, only: [:index, :destroy]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
