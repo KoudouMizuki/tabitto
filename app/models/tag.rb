c128a85200d230240fbd811fd21186e705a5f76e
KoudouMizuki <k.mizuki0706@gmail.com>
2023-02-04 05:54:32 +0000

    Update

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
-  
-  validates :tag, presence: true
+
+  validates :name, presence: true
 
   has_many :post_tags, dependent: :destroy, foreign_key: 'tag_id'
   has_many :posts, through: :post_tags
